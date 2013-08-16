64f1290f9bb5ef00796b8a75a60fcf9e3abb1140
Cristian Mamani <cristian.mamani@supersoftinc.com>
2013-08-16 13:35:11 -0400

    Initial commit

---
+++ install_ubuntu_11_10.sh
@@ -0,0 +1,15 @@
+# /usr/bash!
+
+sudo apt-get update &&
+sudo apt-get upgrade &&
+sudo apt-get install vim -y &&
+sudo apt-get install git -y &&
+sudo apt-get install apache2 -y &&
+sudo apt-get install mysql-server -y  &&
+sudo apt-get install php5 libapache2-mod-php5 php5-mysql php5-gd -y &&
+sudo apt-get install php5-curl -y &&
+sudo apt-get install openjdk-7-jdk -y &&
+sudo apt-get install filezilla -y &&
+sudo apt-get install gnome-session-fallback -y 
+
+
